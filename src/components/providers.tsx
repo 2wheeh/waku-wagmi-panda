46b08eed52793f573f27ee65f1c7aac8f51eaeff
Wonhee Lee <2wheeh@gmail.com>
2026-01-21 02:19:43 +0900

    fix: init queryClient on mount

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,16 +5,18 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 
 import { config } from '../lib/wagmi';
 
-const queryClient = new QueryClient();
+export const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
 
-export const Providers = ({ children }: PropsWithChildren) => (
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <RainbowKitProvider>{children}</RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
-);
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
